9aae1e268a7cf070a45f59638f41e2f1a5fe763f
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-08-20 12:17:43 -0400

    Initial commit

---
+++ mouse_events.py
@@ -0,0 +1,37 @@
+# A simple drawing program
+
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+# outputs mouseclick information
+def mouse_press(event):
+    global prev
+    prev = event
+
+    print('type: {}'.format(event.type))
+    print('widget: {}'.format(event.widget))
+    print('num: {}'.format(event.num))
+    print('x: {}'.format(event.x))
+    print('y: {}'.format(event.y))
+    print('x root: {}'.format(event.x_root))
+    print('y root: {}'.format(event.y_root))
+
+
+# draws a line
+def draw(event):
+    global prev
+
+    canvas.create_line(prev.x, prev.y, event.x, event.y, width=4)
+    prev = event
+
+
+canvas = Canvas(root, width=680, height=480, background='white')
+canvas.pack()
+
+canvas.bind('<ButtonPress>', mouse_press)
+canvas.bind('<B1-Motion>', draw)
+
+if __name__ == '__main__':
+    root.mainloop()
