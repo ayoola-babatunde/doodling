1c256b477a2056eaaea79ba042b5813502440103
Ayoola Babatunde <ayoola@minerva.kgi.edu>
2019-11-21 18:47:01 +0100

    Update mouse_events.py

--- mouse_events.py
+++ mouse_events.py
@@ -20,20 +20,69 @@ def mouse_press(event):
     print('y root: {}'.format(event.y_root))
 
 
-# draws a line
-def draw(event):
+#draws a black
+def drawblack(event):
     global prev
 
-    canvas.create_line(prev.x, prev.y, event.x, event.y, width=4)
+    canvas.create_line(prev.x, prev.y, event.x, event.y, width=4, fill = 'black')
+    prev = event
+
+# draws a red line
+def drawred(event):
+    global prev
+
+    canvas.create_line(prev.x, prev.y, event.x, event.y, width=4, fill = 'red')
     prev = event
 
 
+#close the program
+class quitButton(Button):
+    def __init__(self, parent):
+        Button.__init__(self, parent)
+        self['text'] = 'Good Bye'
+        # Command to close the window (the destory method)
+        self['command'] = parent.destroy
+        self.pack(side=TOP)
+
+quitButton(root)
+
+#in progress
+#change to red
+#class changered(Button):
+#    def __init__(self, parent):
+#        Button.__init__(self, parent)
+#        self['text'] = 'RED'
+#        # Command to close the window (the destory method)
+#        self['command'] =
+#        self.pack(side=TOP)
+
+#quitButton(root)
+
+#clears the screen
+def clearscreen():
+    canvas.delete("all")
+
+#button to clear screen
+clearscrbutton = Button(root, text ="Cl", command = clearscreen)
+
+
+#change pen colour
+def redpen():
+    canvas.bind('<B1-Motion>', drawred)
+
+#button to change to red
+redpenbutton = Button(root, text ="RED", command = redpen)
+
 
 canvas = Canvas(root, width=1920, height=1080, background='white')
+clearscrbutton.pack()
+redpenbutton.pack()
+#clearscrbutton.place(height=100, width=200)
 canvas.pack()
 
+
 canvas.bind('<ButtonPress>', mouse_press)
-canvas.bind('<B1-Motion>', draw)
+canvas.bind('<B1-Motion>', drawblack)
 
 if __name__ == '__main__':
     root.mainloop()
