05b6941a1a485c6e443c3a3717f7007c0a8918dd
Ayoola Babatunde <ayoola@minerva.kgi.edu>
2020-03-02 12:25:32 -0300

    Update mouse_events.py

--- mouse_events.py
+++ mouse_events.py
@@ -1,86 +1,160 @@
-# A simple drawing program
-
 from tkinter import *
 from tkinter import ttk
 
+#root
 root = Tk()
-root.attributes('-alpha', 0.3)
+
+#adjust opacity
+root.attributes('-alpha', 0.9)
+
+top = Frame(root)
+top.pack(side = TOP)
+
+
 
 # outputs mouseclick information
 def mouse_press(event):
     global prev
     prev = event
 
-    print('type: {}'.format(event.type))
-    print('widget: {}'.format(event.widget))
-    print('num: {}'.format(event.num))
-    print('x: {}'.format(event.x))
-    print('y: {}'.format(event.y))
-    print('x root: {}'.format(event.x_root))
-    print('y root: {}'.format(event.y_root))
-
-
-#draws a black
-def drawblack(event):
-    global prev
-
-    canvas.create_line(prev.x, prev.y, event.x, event.y, width=4, fill = 'black')
-    prev = event
-
 # draws a red line
 def drawred(event):
     global prev
-
     canvas.create_line(prev.x, prev.y, event.x, event.y, width=4, fill = 'red')
     prev = event
 
+# draws a orange line
+def draworange(event):
+    global prev
+    canvas.create_line(prev.x, prev.y, event.x, event.y, width=4, fill = 'orange')
+    prev = event
 
-#close the program
+# draws a yellow line
+def drawyellow(event):
+    global prev
+    canvas.create_line(prev.x, prev.y, event.x, event.y, width=4, fill = 'yellow')
+    prev = event
+
+# draws a green line
+def drawgreen(event):
+    global prev
+    canvas.create_line(prev.x, prev.y, event.x, event.y, width=4, fill = 'green')
+    prev = event
+
+# draws a blue line
+def drawblue(event):
+    global prev
+    canvas.create_line(prev.x, prev.y, event.x, event.y, width=4, fill = 'blue')
+    prev = event
+
+# draws a indigo line
+def drawindigo(event):
+    global prev
+    canvas.create_line(prev.x, prev.y, event.x, event.y, width=4, fill = 'indigo')
+    prev = event
+
+# draws a black line
+def drawblack(event):
+    global prev
+    canvas.create_line(prev.x, prev.y, event.x, event.y, width=4, fill = 'black')
+    prev = event
+
+
+
+
+
+#change pen colour to red
+def redpen():
+    canvas.bind('<B1-Motion>', drawred)
+
+#change pen colour to orange
+def orangepen():
+    canvas.bind('<B1-Motion>', draworange)
+
+#change pen colour to yellow
+def yellowpen():
+    canvas.bind('<B1-Motion>', drawyellow)
+
+#change pen colour to green
+def greenpen():
+    canvas.bind('<B1-Motion>', drawgreen)
+
+#change pen colour to blue
+def bluepen():
+    canvas.bind('<B1-Motion>', drawblue)
+
+#change pen colour to indigo
+def indigopen():
+    canvas.bind('<B1-Motion>', drawindigo)
+
+#change pen colour to black
+def blackpen():
+    canvas.bind('<B1-Motion>', drawblack)
+
+
+
+
+
+#button to change to red
+redpenbutton = Button(root, text = u'\u270E', command = redpen, bg = 'red')
+
+#button to change to orange
+orangepenbutton = Button(root, text = u'\u270E', command = orangepen, bg = 'orange')
+
+#button to change to yellow
+yellowpenbutton = Button(root, text = u'\u270E', command = yellowpen, bg = 'yellow')
+
+#button to change to green
+greenpenbutton = Button(root, text = u'\u270E', command = greenpen, bg = 'green')
+
+#button to change to blue
+bluepenbutton = Button(root, text = u'\u270E', command = bluepen, bg = 'blue')
+
+#button to change to indigo
+indigopenbutton = Button(root, text = u'\u270E', command = indigopen, bg = 'indigo')
+
+#button to change to black
+blackpenbutton = Button(root, text = u'\u270E', command = blackpen, bg = 'black', fg = 'white')
+
+
+
+#button to close window
 class quitButton(Button):
     def __init__(self, parent):
         Button.__init__(self, parent)
-        self['text'] = 'Good Bye'
-        # Command to close the window (the destory method)
+        self['text'] = u'\u274C'
+        self['bg'] = 'red'
         self['command'] = parent.destroy
-        self.pack(side=TOP)
+        self.pack(in_ = top, side = RIGHT)
 
 quitButton(root)
 
-#in progress
-#change to red
-#class changered(Button):
-#    def __init__(self, parent):
-#        Button.__init__(self, parent)
-#        self['text'] = 'RED'
-#        # Command to close the window (the destory method)
-#        self['command'] =
-#        self.pack(side=TOP)
 
-#quitButton(root)
 
 #clears the screen
 def clearscreen():
     canvas.delete("all")
 
 #button to clear screen
-clearscrbutton = Button(root, text ="Cl", command = clearscreen)
+clearscrbutton = Button(root, text = 'CLEAR', command = clearscreen, bg = 'white')
 
 
-#change pen colour
-def redpen():
-    canvas.bind('<B1-Motion>', drawred)
+#packs buttons
+blackpenbutton.pack(in_ = top, side = LEFT)
+redpenbutton.pack(in_ = top, side = LEFT)
+orangepenbutton.pack(in_ = top, side = LEFT)
+yellowpenbutton.pack(in_ = top, side = LEFT)
+greenpenbutton.pack(in_ = top, side = LEFT)
+bluepenbutton.pack(in_ = top, side = LEFT)
+indigopenbutton.pack(in_ = top, side = LEFT)
+clearscrbutton.pack(in_ = top, side = LEFT)
 
-#button to change to red
-redpenbutton = Button(root, text ="RED", command = redpen)
 
 
+#canvas init
 canvas = Canvas(root, width=1920, height=1080, background='white')
-clearscrbutton.pack()
-redpenbutton.pack()
-#clearscrbutton.place(height=100, width=200)
+
 canvas.pack()
-
-
 canvas.bind('<ButtonPress>', mouse_press)
 canvas.bind('<B1-Motion>', drawblack)
 
